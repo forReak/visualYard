8ea2d5bb700a633c81f78e41c40b9ce3c3d27677
furao <804849460@qq.com>
2019-02-27 14:27:48 +0800

    此版本集合了thymeleaf，正确配置了spring

--- src/main/java/com/furao/visualYard/controller/HomePageController.java
+++ src/main/java/com/furao/visualYard/controller/HomePageController.java
@@ -1,14 +1,15 @@
 package com.furao.visualYard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomePageController {
 
-    @RequestMapping("/index")
-    public String index(){
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("name","zhangsan");
         return "index";
     }
- 
 }
